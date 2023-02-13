40bb2c15a732443a0c29b3e252165a57aef413ea
User <asanbekovmalik2@gmail.com>
2023-02-13 18:00:51 +0600

    Initial commit

---
+++ src/Circle.java
@@ -0,0 +1,13 @@
+public class Circle {
+    private static double PI=3.14159265359;
+
+    public static void area(double radius){
+        System.out.println( PI*(radius*radius));
+    }
+    public static void circumference(double radius){
+        System.out.println(PI*2*radius);
+    }
+
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+
+Circle.area(45);
+Circle.circumference(55);
+    }}
